b0095e511c69defe1de84f908f1a437f78466018
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2020-07-03 13:05:06 +0200

    Update print bed size so gain a max of 220x225x210, which is within the limits of the hardware.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1176,16 +1176,16 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 215
-#define Y_BED_SIZE 215
+#define X_BED_SIZE 225
+#define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -5
+#define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 205
+#define Z_MAX_POS 210
 
 /**
  * Software Endstops
