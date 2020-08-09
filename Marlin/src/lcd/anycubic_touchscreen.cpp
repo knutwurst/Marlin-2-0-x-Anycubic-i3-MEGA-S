25364c7f2ffc970e3d7e4dd59f79152882991224
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2020-08-09 15:51:44 +0200

    More convinient BLTouch leveling with auto saving and homing.

--- Marlin/src/lcd/anycubic_touchscreen.cpp
+++ Marlin/src/lcd/anycubic_touchscreen.cpp
@@ -538,7 +538,7 @@ void AnycubicTouchscreenClass::HandleSpecialMenu()
   || (strcasestr(currentTouchscreenSelection, SM_BLTOUCH_S) != NULL))
   {
     SERIAL_ECHOLNPGM("Special Menu: BLTouch Leveling");
-    queue.inject_P(PSTR("G28\nG29"));
+    queue.inject_P(PSTR("G28\nG29\nM500\nG90\nG1 X0 Z30 F4000\nG91"));
   }
 #endif
 
