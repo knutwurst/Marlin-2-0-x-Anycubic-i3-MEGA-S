b00040c25d6e86eb868a8e464ec16cc75ac198a7
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2020-08-10 17:14:23 +0200

    Improve deployment script

--- deploy_firmware_files.sh
+++ deploy_firmware_files.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-CURRENT_VERSION="1.1.1"
+VERSION_FILE="/Users/OKoester/Documents/Arduino/Marlin-2-0-x-Anycubic-i3-MEGA-S-Master/Marlin/src/inc/Version.h"
 FIRMWARE_FOLDER="/Users/OKoester/Documents/Arduino/Marlin-2-0-x-Anycubic-i3-MEGA-S-Master/.pio/build/"
 OUTPUT_FOLDER="/Users/OKoester/Desktop/i3_FIRMWARE"
 
-mkdir $OUTPUT_FOLDER
+if [ -d "$OUTPUT_FOLDER" ]; then
+    echo "$OUTPUT_FOLDER already exists."
+else 
+    mkdir $OUTPUT_FOLDER
+fi
+
+CUSTOM_BUILD_VERSION=$(egrep -o "([0-9]{1,}\.)+[0-9]{1,}" $VERSION_FILE -m2 | tail -n1)
+
+echo "Knutwurst's Mega Firmware Version: $CUSTOM_BUILD_VERSION"
 
 cd $FIRMWARE_FOLDER
-for dir in $FIRMWARE_FOLDER/*/     # list directories in the form "/tmp/dirname/"
+for dir in $FIRMWARE_FOLDER/*/
 do
-    dir=${dir%*/}      # remove the trailing "/"
-    echo ${dir##*/}    # print everything after the final "<--/"
-    cp ${dir##*/}/firmware.hex /$OUTPUT_FOLDER/${dir##*/}_v$CURRENT_VERSION.hex
+    dir=${dir%*/}
+    echo ${dir##*/}
+    cp ${dir##*/}/firmware.hex /$OUTPUT_FOLDER/${dir##*/}_v$CUSTOM_BUILD_VERSION.hex
 done
