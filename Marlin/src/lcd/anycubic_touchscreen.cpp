f663765c7ed009d4f0dffc37a527527b7666dcda
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2020-08-21 23:52:11 +0200

    Add detailed Version Info for DGUS II.

--- Marlin/src/lcd/anycubic_touchscreen.cpp
+++ Marlin/src/lcd/anycubic_touchscreen.cpp
@@ -1646,6 +1646,7 @@ void AnycubicTouchscreenClass::GetCommandFromTFT()
         case 33: // A33 get version info
         {
           HARDWARE_SERIAL_PROTOCOLPGM("J33 ");
+          HARDWARE_SERIAL_PROTOCOLPGM("Knutwurst-");
           HARDWARE_SERIAL_PROTOCOLPGM(MSG_MY_VERSION);
           HARDWARE_SERIAL_ENTER();
         }
