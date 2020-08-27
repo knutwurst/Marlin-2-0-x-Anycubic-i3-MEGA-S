0757a5712258e8d579e89e3eaabe566c57afd6fa
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2020-08-27 08:44:46 +0200

    Update Version to 1.1.5 for next release

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -41,10 +41,10 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
- #define CUSTOM_BUILD_VERSION "1.1.5-beta"
+ #define CUSTOM_BUILD_VERSION "1.1.5"
 
 #ifndef STRING_DISTRIBUTION_DATE
-  #define STRING_DISTRIBUTION_DATE "2020-08-26"
+  #define STRING_DISTRIBUTION_DATE "2020-08-27"
 #endif
 
 /**
