d7ab58ea886d4d5c087e4b0f62a12c5938d81779
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2020-09-04 20:55:50 +0200

    Switch Temp Sensor from 5 to 11 which should be the correct one.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -551,7 +551,7 @@
  *   998 : Dummy Table that ALWAYS reads 25°C or the temperature defined below.
  *   999 : Dummy Table that ALWAYS reads 100°C or the temperature defined below.
  */
-#define TEMP_SENSOR_0 5
+#define TEMP_SENSOR_0 11 // 5 is Anycubic default, but the documentation says 11
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
