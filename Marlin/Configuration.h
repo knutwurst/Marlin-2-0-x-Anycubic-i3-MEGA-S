c864789c224d5c428e9db5b690de344cee9f5044
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2020-09-27 10:40:54 +0200

    Revert MEGA print bed size.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1492,7 +1492,13 @@
 // @section machine
 
 // The size of the print bed
-#if ENABLED(KNUTWURST_MEGA) || ENABLED(KNUTWURST_MEGA_S) || ENABLED(KNUTWURST_MEGA_P)
+#if ENABLED(KNUTWURST_MEGA)
+    #define X_BED_SIZE 210
+    #define Y_BED_SIZE 210
+    #define Z_BED_HEIGHT 210
+#endif
+
+#if ENABLED(KNUTWURST_MEGA_S) || ENABLED(KNUTWURST_MEGA_P)
     #define X_BED_SIZE 225
     #define Y_BED_SIZE 220
     #define Z_BED_HEIGHT 210
