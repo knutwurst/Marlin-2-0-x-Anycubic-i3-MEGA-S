da2b3d1a6a1829cb224c853874fc1f37d4b2a3a2
mbo <mb@qapp.de>
2020-11-04 09:36:21 +0100

    switched to dynamic paths, pushd popd instead of cd, make dir recursively

--- deploy_firmware_files.sh
+++ deploy_firmware_files.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-VERSION_FILE="/Users/OKoester/Documents/Arduino/Marlin-2-0-x-Anycubic-i3-MEGA-S-Master/Marlin/src/inc/Version.h"
-FIRMWARE_FOLDER="/Users/OKoester/Documents/Arduino/Marlin-2-0-x-Anycubic-i3-MEGA-S-Master/.pio/build/"
-OUTPUT_FOLDER="/Users/OKoester/Desktop/i3_FIRMWARE"
+BASE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CURRENT_USER="$(whoami)"
+
+VERSION_FILE="$BASE_PATH/Marlin/src/inc/Version.h"
+FIRMWARE_FOLDER="$BASE_PATH/.pio/build/"
+OUTPUT_FOLDER="/Users/$CURRENT_USER/Desktop/i3_FIRMWARE"
 
 CUSTOM_BUILD_VERSION=$(egrep -o "([0-9]{1,}\.)+[a-zA-Z0-9_.-]{1,}" $VERSION_FILE -m2 | tail -n1)
 
 if [ -d "$$OUTPUT_FOLDER/v$CUSTOM_BUILD_VERSION" ]; then
     echo "$$OUTPUT_FOLDER/$CUSTOM_BUILD_VERSION already exists."
 else 
-    mkdir $OUTPUT_FOLDER/$CUSTOM_BUILD_VERSION
+    mkdir -p $OUTPUT_FOLDER/$CUSTOM_BUILD_VERSION
 fi
 
 echo "Knutwurst's Mega Firmware Version: $CUSTOM_BUILD_VERSION"
 
-cd $FIRMWARE_FOLDER
-for dir in $FIRMWARE_FOLDER/*/
-do
-    dir=${dir%*/}
-    echo ${dir##*/}
-    cp ${dir##*/}/firmware.hex /$OUTPUT_FOLDER/$CUSTOM_BUILD_VERSION/${dir##*/}_v$CUSTOM_BUILD_VERSION.hex
-done
+pushd $FIRMWARE_FOLDER
+  for dir in $FIRMWARE_FOLDER/*/
+  do
+      dir=${dir%*/}
+      echo ${dir##*/}
+      cp ${dir##*/}/firmware.hex /$OUTPUT_FOLDER/$CUSTOM_BUILD_VERSION/${dir##*/}_v$CUSTOM_BUILD_VERSION.hex
+  done
+popd
