5770b99ff5b75cc7f34a1096cf8fd0a9df2bcb67
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2020-11-29 12:50:11 +0100

    Increase max temperatures and set distribution date for release

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -41,10 +41,10 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
- #define CUSTOM_BUILD_VERSION "1.1.7-BETA3"
+ #define CUSTOM_BUILD_VERSION "1.1.7"
 
 #ifndef STRING_DISTRIBUTION_DATE
-  #define STRING_DISTRIBUTION_DATE "2020-11-24"
+  #define STRING_DISTRIBUTION_DATE "2020-11-29"
 #endif
 
 /**
