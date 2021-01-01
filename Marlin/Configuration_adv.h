53cdbaec5864167d974376ca1179b0fd7becd1e8
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2021-01-01 15:11:43 +0100

    Decrease stepper dir delay to eliminate direction bug for extruders with TMC stepper drivers.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1702,8 +1702,8 @@
  *
  * Override the default value based on the driver type set in Configuration.h.
  */
-#define MINIMUM_STEPPER_POST_DIR_DELAY 650
-#define MINIMUM_STEPPER_PRE_DIR_DELAY 650
+#define MINIMUM_STEPPER_POST_DIR_DELAY 200
+#define MINIMUM_STEPPER_PRE_DIR_DELAY 200
 
 /**
  * Minimum stepper driver pulse width (in µs)
