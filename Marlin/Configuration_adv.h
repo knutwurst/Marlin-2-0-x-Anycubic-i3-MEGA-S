6ed85e90d0d3b6d9b29538734febe96fe0b59a36
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2021-04-10 15:05:10 +0200

    Fix non working extruder fan on Mega P and Chiron.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -424,7 +424,7 @@
  * the fan will turn on when any selected extruder is above the threshold.
  */
 
-#if ANY(KNUTWURST_MEGA, KNUTWURST_MEGA_S, KNUTWURST_MEGA_X)
+#if ANY(KNUTWURST_MEGA, KNUTWURST_MEGA_S, KNUTWURST_MEGA_X, KNUTWURST_MEGA_P, KNUTWURST_CHIRON)
     #define E0_AUTO_FAN_PIN FAN2_PIN
     #define E1_AUTO_FAN_PIN -1
     #define E2_AUTO_FAN_PIN -1
@@ -433,7 +433,7 @@
     #define E5_AUTO_FAN_PIN -1
     #define CHAMBER_AUTO_FAN_PIN -1
 #endif
-
+/*
 #if ENABLED(KNUTWURST_CHIRON) // this might need some rework
     #define E0_AUTO_FAN_PIN -1
     #define E1_AUTO_FAN_PIN -1
@@ -443,7 +443,7 @@
     #define E5_AUTO_FAN_PIN -1
     #define CHAMBER_AUTO_FAN_PIN -1
 #endif
-
+*/
 #define EXTRUDER_AUTO_FAN_TEMPERATURE 50
 #define EXTRUDER_AUTO_FAN_SPEED 255   // 255 == full speed
 #define CHAMBER_AUTO_FAN_TEMPERATURE 30
