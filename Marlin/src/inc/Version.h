cb4eafedb67d022c3ef91754965290406e2a0f7c
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2021-04-21 12:50:20 +0200

    Bump version to Beta 2

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -41,7 +41,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
- #define CUSTOM_BUILD_VERSION "1.2.0-b1hotfix2"
+ #define CUSTOM_BUILD_VERSION "1.2.0-beta.2"
 
 #ifndef STRING_DISTRIBUTION_DATE
   #define STRING_DISTRIBUTION_DATE "2021-04-21"
