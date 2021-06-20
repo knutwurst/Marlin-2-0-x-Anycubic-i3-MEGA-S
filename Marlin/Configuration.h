6437bbfe91b55b89db7f05600887b4164950b748
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2021-06-20 12:46:32 +0200

    Reset temp values to maximum possible and fix typo.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -529,15 +529,15 @@
 // Above this temperature the heater will be switched off.
 // This can protect components from overheating, but NOT from shorts and failures.
 // (Use MINTEMP for thermistor short/failure protection.)
-#define HEATER_0_MAXTEMP 400
-#define HEATER_1_MAXTEMP 400
-#define HEATER_2_MAXTEMP 400
-#define HEATER_3_MAXTEMP 400
-#define HEATER_4_MAXTEMP 400
-#define HEATER_5_MAXTEMP 400
-#define HEATER_6_MAXTEMP 400
-#define HEATER_7_MAXTEMP 400
-#define BED_MAXTEMP      180
+#define HEATER_0_MAXTEMP 275
+#define HEATER_1_MAXTEMP 275
+#define HEATER_2_MAXTEMP 275
+#define HEATER_3_MAXTEMP 275
+#define HEATER_4_MAXTEMP 275
+#define HEATER_5_MAXTEMP 275
+#define HEATER_6_MAXTEMP 275
+#define HEATER_7_MAXTEMP 275
+#define BED_MAXTEMP      150
 
 //===========================================================================
 //============================= PID Settings ================================
