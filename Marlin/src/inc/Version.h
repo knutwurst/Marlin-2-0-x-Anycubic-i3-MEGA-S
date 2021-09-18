3eff63185dc903bf322a46b8161fdab41c981533
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2021-09-18 12:35:29 +0200

    Set correct version

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -41,10 +41,10 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
- #define CUSTOM_BUILD_VERSION "1.3.0-beta.3"
+ #define CUSTOM_BUILD_VERSION "1.3.0"
 
 #ifndef STRING_DISTRIBUTION_DATE
-  #define STRING_DISTRIBUTION_DATE "2021-09-14"
+  #define STRING_DISTRIBUTION_DATE "2021-09-18"
 #endif
 
 /**
