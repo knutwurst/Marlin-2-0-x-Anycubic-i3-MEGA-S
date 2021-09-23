21d062c285e37d0397c3926a36fadea8f04f8615
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2021-09-23 18:37:47 +0200

    Disable Wifi for 1.3.1 Release

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -41,10 +41,10 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
- #define CUSTOM_BUILD_VERSION "1.3.0"
+ #define CUSTOM_BUILD_VERSION "1.3.1"
 
 #ifndef STRING_DISTRIBUTION_DATE
-  #define STRING_DISTRIBUTION_DATE "2021-09-18"
+  #define STRING_DISTRIBUTION_DATE "2021-09-23"
 #endif
 
 /**
