1707addea754af45defd6e885fa52601979985f7
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2021-11-02 11:02:35 +0100

    bump version for nex beta release

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -41,10 +41,10 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
- #define CUSTOM_BUILD_VERSION "1.3.2"   // PATCH: Knutwurst
+ #define CUSTOM_BUILD_VERSION "1.4.0-b1"
 
 #ifndef STRING_DISTRIBUTION_DATE
-  #define STRING_DISTRIBUTION_DATE "2021-09-23"
+  #define STRING_DISTRIBUTION_DATE "2021-11-02"
 #endif
 
 /**
