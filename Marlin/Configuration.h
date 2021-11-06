35ea8497b0555f04a3aacaee85638651c377a9dc
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2021-11-06 21:50:09 +0100

    Fine tune acceleration, jerk and feedrate settings for all printers especially 4MAX Pro

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1128,11 +1128,11 @@
 #endif
 
 #if ENABLED(KNUTWURST_4MAX)
-  #define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 80, 800, 422 }
+  #define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 80, 800, 415 }
 #endif
 
 #if ENABLED(KNUTWURST_4MAXP2)
-  #define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 80, 800, 422 }
+  #define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 80, 800, 415 }
 #endif
 
 /**
@@ -1144,7 +1144,7 @@
   #if ENABLED(KNUTWURST_BMG)
     #define DEFAULT_MAX_FEEDRATE          { 500, 500, 6, 30 }
   #else
-    #define DEFAULT_MAX_FEEDRATE          { 500, 500, 6, 60 }
+    #define DEFAULT_MAX_FEEDRATE          { 500, 500, 6, 80 }
   #endif
 #endif
 
@@ -1157,7 +1157,6 @@
     #define DEFAULT_MAX_FEEDRATE            { 120, 120, 18, 30 } // correct for BMG?
   #else
     #define DEFAULT_MAX_FEEDRATE            { 120, 120, 18, 80 } // thanks to Simon Geis
-    //#define DEFAULT_MAX_FEEDRATE            { 400, 400, 18, 80 } // thanks to DanJunior78
   #endif
 #endif
 
@@ -1174,7 +1173,7 @@
 #endif
 
 #if ENABLED(KNUTWURST_4MAX)
-  #define DEFAULT_MAX_FEEDRATE          { 150, 150, 25, 30 }
+  #define DEFAULT_MAX_FEEDRATE          { 150, 150, 25, 80 }
 #endif
 
 #if ENABLED(KNUTWURST_4MAXP2)
@@ -1207,11 +1206,11 @@
 #endif
 
 #if ENABLED(KNUTWURST_4MAX)
- #define DEFAULT_MAX_ACCELERATION      { 2000, 2000, 60, 10000 }
+ #define DEFAULT_MAX_ACCELERATION      { 1500, 1500, 100, 10000 }
 #endif
 
 #if ENABLED(KNUTWURST_4MAXP2)
- #define DEFAULT_MAX_ACCELERATION      { 2000, 2000, 60, 10000 }
+ #define DEFAULT_MAX_ACCELERATION      { 1500, 1500, 70, 15000 }
 #endif
 
 
@@ -1271,9 +1270,9 @@
 #endif
 
 #if ENABLED(KNUTWURST_4MAXP2)
-  #define DEFAULT_ACCELERATION          1500    // X, Y, Z and E acceleration for printing moves
+  #define DEFAULT_ACCELERATION          800    // X, Y, Z and E acceleration for printing moves
   #define DEFAULT_RETRACT_ACCELERATION  1500    // E acceleration for retracts
-  #define DEFAULT_TRAVEL_ACCELERATION   1500    // X, Y, Z acceleration for travel (non printing) moves
+  #define DEFAULT_TRAVEL_ACCELERATION   1000    // X, Y, Z acceleration for travel (non printing) moves
 #endif
 
 /**
@@ -1300,10 +1299,16 @@
     #define DEFAULT_ZJERK  0.2
   #endif
 
-  #if ANY(KNUTWURST_CHIRON, KNUTWURST_4MAX, KNUTWURST_4MAXP2)
+  #if ENABLED(KNUTWURST_CHIRON)
     #define DEFAULT_XJERK  4.0
     #define DEFAULT_YJERK  4.0
     #define DEFAULT_ZJERK  0.4
+  #endif
+
+  #if ANY(KNUTWURST_4MAX, KNUTWURST_4MAXP2)
+    #define DEFAULT_XJERK  8.0
+    #define DEFAULT_YJERK  8.0
+    #define DEFAULT_ZJERK  0.2
   #endif
 
   //#define TRAVEL_EXTRA_XYJERK 0.0     // Additional jerk allowance for all travel moves
