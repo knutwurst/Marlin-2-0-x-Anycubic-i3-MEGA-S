515df460aef7cffd5feb8ea91a78129f9768af12
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2021-11-16 12:13:33 +0100

    BLTouch Config for 4MAX Pro which uses Z-Min-Pin

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1373,7 +1373,9 @@
  * The probe replaces the Z-MIN endstop and is used for Z homing.
  * (Automatically enables USE_PROBE_FOR_Z_HOMING.)
  */
-//#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
+#if ENABLED(KNUTWURST_4MAXP2)
+  #define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
+#endif
 
 // Force the use of the probe for Z-axis homing
 //#define USE_PROBE_FOR_Z_HOMING
