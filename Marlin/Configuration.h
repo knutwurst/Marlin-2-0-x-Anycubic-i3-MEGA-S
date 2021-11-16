d0219c5632bb9e55ce1313c85d6eca6aa6ae6f28
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2021-11-16 12:22:44 +0100

    Add Z_MIN_ENDSTOP inversion logic

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -980,7 +980,11 @@
   // Mechanical endstop with COM to ground and NC to Signal uses "false" here (most common setup).
   #define X_MIN_ENDSTOP_INVERTING true  // Set to true to invert the logic of the endstop.
   #define Y_MIN_ENDSTOP_INVERTING true  // Set to true to invert the logic of the endstop.
-  #define Z_MIN_ENDSTOP_INVERTING true  // Set to true to invert the logic of the endstop.
+  #if BOTH(KNUTWURST_BLTOUCH, KNUTWURST_4MAXP2)
+      #define Z_MIN_ENDSTOP_INVERTING false  // Set to true to invert the logic of the endstop.
+  #else
+      #define Z_MIN_ENDSTOP_INVERTING true  // Set to true to invert the logic of the endstop.
+  #endif
   #define I_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
   #define J_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
   #define K_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
