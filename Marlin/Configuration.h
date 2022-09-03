bd49cc3d483d2940936fe19fa23d93c8b5ac30c9
Stefan Kalscheuer <stefan@stklcode.de>
2022-09-03 09:27:21 +0200

    config: use soft PWM for fans
    The hardware PWM for fan control conflicts with the speaker setup. While
    this has always been an issue, sanity checks now fail because of this.
    
    We've used soft PWM in 1.4.0 and reverted back to hardware PWM in 1.4.1,
    so we probably need some additional investigation and testing here.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -3837,14 +3837,14 @@
 // Use software PWM to drive the fan, as for the heaters. This uses a very low frequency
 // which is not as annoying as with the hardware PWM. On the other hand, if this frequency
 // is too low, you should also increment SOFT_PWM_SCALE.
-// #define FAN_SOFT_PWM
+#define FAN_SOFT_PWM
 
 // Incrementing this by 1 will double the software PWM frequency,
 // affecting heaters, and the fan if FAN_SOFT_PWM is enabled.
 // However, control resolution will be halved for each increment;
 // at zero value, there are 128 effective control positions.
 // :[0,1,2,3,4,5,6,7]
-// #define SOFT_PWM_SCALE 2
+#define SOFT_PWM_SCALE 2
 
 // If SOFT_PWM_SCALE is set to a value higher than 0, dithering can
 // be used to mitigate the associated resolution loss. If enabled,
