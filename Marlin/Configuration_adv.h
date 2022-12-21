c03aa9d5a44679bfb0533a9cc6637abec2526e83
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2022-12-21 08:58:53 +0100

    Fix Z home position not touching the buildplate (see #396) by removing the 2mm HOMING_BACKOFF_POST_MM on z.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -916,7 +916,7 @@
 #define HOMING_BUMP_DIVISOR { 2, 2, 4 }       // Re-Bump Speed Divisor (Divides the Homing Feedrate)
 
 #if DISABLED(KNUTWURST_CHIRON)
-  #define HOMING_BACKOFF_POST_MM { 2, 2, 2 }  // (linear=mm, rotational=°) Backoff from endstops after homing
+  #define HOMING_BACKOFF_POST_MM { 2, 2, 0 }  // (linear=mm, rotational=°) Backoff from endstops after homing
 #endif
 // #define XY_COUNTERPART_BACKOFF_MM 0         // (mm) Backoff X after homing Y, and vice-versa
 
